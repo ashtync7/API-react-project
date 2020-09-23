1785553e0248e123251963959a3d48a3c516d209
Ashtyn Ce <ashtyncza@gmail.com>
2020-09-23 11:41:12 -0400

    wuzgood

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
+import md5 from 'md5'
+
+
+import api from 'marvel-api'
+
+const marvel = api.createClient({
+  publicKey: process.env.REACT_APP_MARVELKEY
+  , privateKey: process.env.REACT_APP_PRIVKEY
+});
+
+
+
+// developer.marvel.com
 
 function App() {
+  console.log(process.env)
+  const getDD = async () => {
+    let res = await Axios.get('https://www.dnd5eapi.co/api/spells/acid-arrow/')
+    console.log(res)
+  }
+
+  const getMarvel = async () => {
+    let res = await marvel.characters.findAll()
+    //let res = await marvel.events.findByName('spider-man')
+    //let res = await marvel.creators.findByName('Pat', 'Lee', '(X-Men/FF)')
+    console.log(res)
+  }
+
+  const marvelAgain = async () => {
+    let ts = new Date()
+    let res = await Axios.get(`http://gateway.marvel.com/v1/public/comics?ts=${ts}&apikey=${process.env.REACT_APP_MARVELKEY}&hash=${md5(ts + process.env.REACT_APP_PRIVKEY + process.env.REACT_APP_MARVELKEY)}`)
+    console.log(res)
+  }
+  marvelAgain()
+
+
+
+  getDD()
+  getMarvel()
   return (
     <div className="App">
       <header className="App-header">
@@ -22,5 +60,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
