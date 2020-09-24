92e6b2f8ff8250a8d40770919da382b58b4b6134
Ashtyn Ce <ashtyncza@gmail.com>
2020-09-24 12:57:20 -0400

    hpstuff

--- src/Components/HarryPotter.js
+++ src/Components/HarryPotter.js
@@ -1,23 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
+import { Link } from 'react-router-dom';
+
+// <form onSubmit={idk what to put here}>
+//     <input type="image" src={'/sortinghat'}></input>
+// </form >
+
+// OR <button onClick={message}> Press me to print a message! </button>
+
 
 function HarryPotter(props) {
     let [hpChar, setHpChar] = useState([]);
+    const getPotter = async () => {
+        let HPres = await Axios.get("https://www.potterapi.com/v1/characters?key=$2a$10$AkgCGEKIUDVqXQ.rjYiMgu3kTgjm4QVSxU9zz188JXV7VPWSZaiOW")
+        console.log(HPres.data)
+        setHpChar(HPres.data)
+    }
     useEffect(() => {
 
-        const getPotter = async () => {
-            let HPres = await Axios.get("https://www.potterapi.com/v1/characters?key=$2a$10$AkgCGEKIUDVqXQ.rjYiMgu3kTgjm4QVSxU9zz188JXV7VPWSZaiOW")
-            console.log(HPres.data)
-            setHpChar(HPres.data)
-        }
+
         getPotter()
     }, [])
 
     console.log(hpChar)
     let random = hpChar[Math.floor(Math.random() * hpChar.length)]
     console.log(random)
+
     return (
+
         <div>
+            <img onClick={getPotter} src="./sortinghat.png" />
             {random?.name},
             {random?.school ? random?.school : 'school unknown'},
             {random?.house ? random?.house : 'no house'},
