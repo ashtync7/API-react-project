104cc258a2b8b7a37dfb207a8d0f9779686138ea
matheus <mtava002@gmail.com>
2020-09-25 11:19:25 -0400

    push push boom boom

--- src/Components/Star2.js
+++ src/Components/Star2.js
@@ -1,2 +1,21 @@
 import React, { useState, useEffect } from 'react'
 import Axios from 'axios'
+
+
+function StarWars2() {
+
+    let [starLink, setStarLink] = useState([])
+
+    useEffect(() => {
+
+        const getSWAPI = async () => {
+            let SWres = await Axios.get('https://swapi.dev/api/')
+            console.log(SWres)
+            setStarLink(Object.keys(SWres.data))
+        }
+        getSWAPI()
+    }, [])
+
+}
+
+export default StarWars2;
